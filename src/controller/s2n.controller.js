62479c57dc83b0e9682bf1ce95a63b5226b22f04
thomas <thomas.chalanson@sfr.fr>
2022-02-17 16:10:14 +0100

    testing update request

--- src/controller/s2n.controller.js
+++ src/controller/s2n.controller.js
@@ -6,15 +6,18 @@ const router = express.Router();
 router.get("/", (req, res) => {
   connection.query(
     `SELECT * FROM entreprises AS e
+
       INNER JOIN entreprises_technos AS et
         ON e.idEntreprises = et.entreprises_id
       INNER JOIN technos AS t
         ON et.technos_id = t.idTechnos
+
       INNER JOIN entreprises_cities AS ec
         ON e.idEntreprises = ec.id_entreprises
       INNER JOIN cities AS c
         ON ec.cities_id = c.idCities
-     ORDER BY s2n_name ASC`,
+     
+      ORDER BY s2n_name ASC`,
     (err, results) => {
       if (err) {
         console.log(err);
@@ -31,14 +34,17 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   connection.query(
     `SELECT * FROM entreprises AS e
-      INNER JOIN entreprises_technos AS et
-        ON e.idEntreprises = et.entreprises_id
-      INNER JOIN technos AS t
-        ON et.technos_id = t.idTechnos
-      INNER JOIN entreprises_cities AS ec
-        ON e.idEntreprises = ec.id_entreprises
-      INNER JOIN cities AS c
-        ON ec.cities_id = c.idCities
+
+    INNER JOIN entreprises_technos AS et
+      ON e.idEntreprises = et.entreprises_id
+    INNER JOIN technos AS t
+      ON et.technos_id = t.idTechnos
+
+    INNER JOIN entreprises_cities AS ec
+      ON e.idEntreprises = ec.id_entreprises
+    INNER JOIN cities AS c
+      ON ec.cities_id = c.idCities
+      
      WHERE idEntreprises = ?`,
     [req.params.id],
     (err, results) => {
@@ -68,12 +74,20 @@ router.get("/:id", (req, res) => {
 router.post("/", (req, res) => {
   const { images, infos, s2n_name, rate, year } = req.body;
   connection.query(
-    "INSERT INTO entreprises (s2n_name, infos, rate, images, year) VALUES ( ?, ?, ?, ?, ? )",
+   // "INSERT INTO entreprises (s2n_name, infos, rate, images, year) VALUES ( ?, ?, ?, ?, ? )",
 
-    // `INSERT INTO entreprises AS e (s2n_name, infos, rate, images, year, citie_name)
-    //   SELECT CONCAT(e.s2n_name,' ',e.infos,' ',e.rate, ' ',e.images,' ', e.year,' ', e.citie_name) FROM e
-    //     INNER JOIN technos As t ON t.idTechnos=e.IdEntreprises
-    //   WHERE e.IdEntreprises= ?`,
+    `INSERT INTO entreprises AS e (s2n_name, infos, rate, images, year, citie_name)
+      SELECT CONCAT(e.s2n_name,' ',e.infos,' ',e.rate, ' ',e.images,' ', e.year,' ', e.citie_name) FROM e    
+        INNER JOIN entreprises_technos AS et
+          ON e.idEntreprises = et.entreprises_id
+        INNER JOIN technos AS t
+          ON et.technos_id = t.idTechnos
+
+        INNER JOIN entreprises_cities AS ec
+          ON e.idEntreprises = ec.id_entreprises
+        INNER JOIN cities AS c
+          ON ec.cities_id = c.idCities
+      WHERE e.IdEntreprises = ?`,
 
     [s2n_name, infos, rate, images, year],
     (error, results) => {
@@ -110,7 +124,14 @@ router.put("/s2n/:id", (req, res) => {
   const newS2n = req.body;
   // req.body permet de modifier toutes les valeurs au lieu de faire comme const {} = req.body
   connection.query(
-    `UPDATE entreprises SET ? WHERE idEntreprises = ?`,
+    `UPDATE entreprises e 
+      INNER JOIN entreprises_cities AS ec
+        ON e.idEntreprises = ec.id_entreprises
+      INNER JOIN cities AS c
+       ON ec.cities_id = c.idCities
+      SET ?
+       WHERE e.idEntreprises = ?`,
+
     [newS2n, idS2n],
     (error, result) => {
       if (error) {
